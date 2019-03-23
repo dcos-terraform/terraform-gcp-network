0831486ce84315e2ea563531f42e5015c624acdd
Roly Vicaria <roly.vicaria@taxfyle.com>
2019-03-23 10:21:40 -0400

    Change modules/resources to use google-beta provider

--- main.tf
+++ main.tf
@@ -17,11 +17,13 @@
  * ```
  */
 
-provider "google" {}
+provider "google-beta" {}
 
 resource "google_compute_network" "network" {
   name                    = "${var.cluster_name}-network"
   auto_create_subnetworks = "false"
+
+  provider = "google-beta"
 }
 
 ## Two subnetworks (masters, agents)
@@ -29,10 +31,14 @@ resource "google_compute_subnetwork" "master-subnet" {
   name          = "${var.cluster_name}-master-subnet"
   ip_cidr_range = "${var.master_cidr_range}"
   network       = "${google_compute_network.network.self_link}"
+
+  provider = "google-beta"
 }
 
 resource "google_compute_subnetwork" "agent-subnet" {
   name          = "${var.cluster_name}-agent-subnet"
   ip_cidr_range = "${var.agent_cidr_range}"
   network       = "${google_compute_network.network.self_link}"
+
+  provider = "google-beta"
 }
